c48134cfeb3e89c33cf032ca1ed0da1607a91490
rsteller <robbiestells@hotmail.com>
2016-12-22 11:55:15 -0500

    Initial commit

---
+++ app/src/free/java/com.example.studio111.flavor/free/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.studio111.flavor.free;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.studio111.flavor.R;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+        tv.setText("Free");
+    }
+}
